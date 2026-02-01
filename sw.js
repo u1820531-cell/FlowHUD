90fc1450fb61652491c805cb2ffef68f2c47b776
Wow <u1820531@gmail.com>
2026-02-01 15:47:11 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,14 @@
 const CACHE_NAME = 'flow-hud-v1';
 const ASSETS = [
+  './',
   './index.html',
   'https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300;400;600;800&family=JetBrains+Mono:wght@400;700&display=swap'
 ];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
